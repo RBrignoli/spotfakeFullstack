cb563bed8a705b95dff4d7f21d41bc36b95e55f2
RBrignoli <ramon.brignoli@gmail.com>
2024-11-11 12:37:08 -0300

    add cloudinary

--- mobile/spotfake/app/profile/index.jsx
+++ mobile/spotfake/app/profile/index.jsx
@@ -1,23 +1,63 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { StyleSheet, View, Text, Image, ActivityIndicator, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, Image, ActivityIndicator, TouchableOpacity, Pressable } from 'react-native'
 import { AppContext } from '../../scripts/appContext';
-
+import * as ImagePicker from 'expo-image-picker';
 
 
 export default profile = () => {
     const { userInfo, setUserInfo } = useContext(AppContext)
-    console.log(userInfo)
+    const [image, setImage] = useState('https://www.jet.ir/uploadFiles/avatar/noprofile.png');
+    const [newImage, setNewImage] = useState(false)
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+        if (!result.canceled) {
+            console.log(result.assets[0])
+            setImage(result.assets[0].uri);
+            setNewImage(true)
+        }
+    };
+
+    const handleSendImage = async () => {
+        try {
+            const data = {
+                "file": image,
+                "upload_preset": 'ml_default',
+                "name": 'teste' //nao funcionou
+            }
+            const res = await fetch('https://api.cloudinary.com/v1_1/drsblkw5n/upload', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                  },
+                body: JSON.stringify(data)
+            });
+            const result = await res.json();
+            console.log(result);
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
 
     return (
         <View style={styles.container}>
             {userInfo ?
                 <View>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={pickImage}>
                         <Image
                             style={styles.logo}
-                            source={{ uri: 'https://www.jet.ir/uploadFiles/avatar/noprofile.png' }}
+                            source={{ uri: image }}
                         />
                     </TouchableOpacity>
+                    {newImage && <Pressable onPress={handleSendImage} style={styles.buttonStyle}>
+                        <Text>Change Image</Text>
+                    </Pressable> }
                     <Text>{userInfo.nome}</Text>
                     <Text>{userInfo.email}</Text>
                     <Text>{userInfo.status}</Text>
@@ -40,5 +80,12 @@ const styles = StyleSheet.create({
         height: 100,
         borderRadius: 20,
     },
+    buttonStyle: {
+        backgroundColor: 'rgb(255, 135, 46)',
+        padding: 10,
+        width: '90%',
+        alignItems: 'center',
+        borderRadius: 10,
+    }
 })
 
