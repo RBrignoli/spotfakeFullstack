d132f1f49c42fd087e2941d9f86476d490ff970f
RBrignoli <ramon.brignoli@gmail.com>
2024-11-22 09:45:53 -0300

    add player and more

--- mobile/spotfake/scripts/appContext.js
+++ mobile/spotfake/scripts/appContext.js
@@ -3,9 +3,11 @@ import { createContext, useState } from "react";
 export const AppContext = createContext()
 export const AppProvider = ({ children }) => {
     const [userInfo, setUserInfo] = useState({})
+    const [playingNow, setPlayingNow] = useState({})
+
     
     return (
-        <AppContext.Provider value={{userInfo, setUserInfo}}>
+        <AppContext.Provider value={{userInfo, setUserInfo, playingNow, setPlayingNow}}>
             {children}
         </AppContext.Provider>
     )
--- server/roteamento/rotas_album.js
+++ server/roteamento/rotas_album.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { pegarTodosAlbums, pegarAlbumPorId, pegarMusicaPeloAlbum } from '../controlador/controlador_album.js';
+import { pegarTodosAlbums, pegarAlbumPorId, pegarMusicaPeloAlbum, getMaisOuvidas } from '../controlador/controlador_album.js';
 
 const rotas_albums = express.Router();
 
 rotas_albums.get('/', pegarTodosAlbums);
+rotas_albums.get('/mais-ouvidas', getMaisOuvidas)
 rotas_albums.get('/:id', pegarAlbumPorId); 
-rotas_albums.get('/:id/musicas/', pegarMusicaPeloAlbum);  
+rotas_albums.get('/:id/musicas/', pegarMusicaPeloAlbum);
 
 export {rotas_albums};
